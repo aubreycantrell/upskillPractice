baa51d64eb244a2052111a6ea2c610a1f362d544
aubreycantrell <aubrey.cantrell@wellesley.edu>
2025-01-15 09:46:17 -0500

    add instance bars constructor to string to snack

--- snacks/Snack.java
+++ snacks/Snack.java
@@ -1,6 +1,27 @@
 public class Snack{
 
+    //instance variables about snacks
+    float price;
+    String[] toppings;
+    int quantity;
+    String name;
+
+    //constructor to make a snack
+    public snack (String name, int quantity, String[] toppings, float price) {
+        this.name = name;
+        this.quantity =  quantity;
+        this.toppings = toppings;
+        this.price = price;
+    }
+
+    //to string to show instance of a snack
+    public String toString(){
+        return "Snack: " + name + ", Quanity: " + quanity + " at price: $" + price;
+    }
+
     public static void main(String[] args){
-        System.out.println(x:"snakcs!");
+        Snack oatmeal = new Snack("oatmeal", 0, new String["brown sugar", "bananas", "cinnamon"], 0.2);
+        System.out.println(oatmeal);
+
     }
 }
